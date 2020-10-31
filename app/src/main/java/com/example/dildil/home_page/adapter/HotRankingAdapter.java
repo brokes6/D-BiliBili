d3fdb23b189f95bcd99204555e4af318a7cfe66e
brokes6 <brokes6@users.noreply.github.com>
2020-10-31 18:23:34 +0800

    提交剩下的

--- app/src/main/java/com/example/dildil/home_page/adapter/HotRankingAdapter.java
+++ app/src/main/java/com/example/dildil/home_page/adapter/HotRankingAdapter.java
@@ -2,60 +2,119 @@ package com.example.dildil.home_page.adapter;
 
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.liuzhuang.rcimageview.RoundCornerImageView;
+import com.blankj.utilcode.util.ActivityUtils;
 import com.bumptech.glide.Glide;
+import com.example.customcontrollibs.BaseAdapter;
 import com.example.dildil.R;
 import com.example.dildil.home_page.bean.RecommendVideoBean;
-import com.xuexiang.xui.adapter.recyclerview.BaseRecyclerAdapter;
-import com.xuexiang.xui.adapter.recyclerview.RecyclerViewHolder;
+import com.example.dildil.home_page.view.RankingLstActivity;
+import com.example.dildil.util.DensityUtil;
 
-public class HotRankingAdapter extends BaseRecyclerAdapter<RecommendVideoBean.BeanData>{
-    private static final String TAG = "HotRankingAdapter";
+public class HotRankingAdapter extends BaseAdapter<RecommendVideoBean.BeanData, RecyclerView.ViewHolder> {
     private Context mContext;
-    private RoundCornerImageView video_cover;
-    private RelativeLayout hot_video_cover;
+    private int position;
     private ItemOnClickListener listener;
-    public HotRankingAdapter(Context context){
+
+    public HotRankingAdapter(Context context) {
         mContext = context;
     }
-    @Override
-    protected int getItemLayoutId(int viewType) {
-        return R.layout.item_hot_ranking_list;
-    }
-
 
     public void setListener(ItemOnClickListener listener) {
         this.listener = listener;
     }
 
+
+    @NonNull
     @Override
-    protected void bindData(@NonNull RecyclerViewHolder holder, int position, RecommendVideoBean.BeanData item) {
-        video_cover = holder.findViewById(R.id.Ranking_video_img);
-        hot_video_cover = holder.findViewById(R.id.Hot_video_cover);
-        hot_video_cover.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (listener != null) {
-                    listener.onClick(getSelectPosition(),item.getId());
+    protected RecyclerView.ViewHolder getViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (mHeaderView != null && viewType == TYPE_HEADER) {
+            return new HotHeaderViewHolder(inflateView(parent, R.layout.item_hot_header_list));
+        } else {
+            return new HotViewHolder(inflateView(parent, R.layout.item_hot_ranking_list));
+        }
+    }
+
+    @Override
+    protected void bindData(@NonNull RecyclerView.ViewHolder holder, int position, RecommendVideoBean.BeanData item) {
+        if (getItemViewType(position) == TYPE_HEADER) {
+            ((HotHeaderViewHolder)holder).rankingList.setTag(position);
+        }else{
+            Glide.with(mContext)
+                    .load(item.getCover())
+                    .placeholder(R.drawable.skeleton_circular_grey)
+                    .into(((HotViewHolder)holder).video_cover);
+            ((HotViewHolder)holder).title.setText(item.getTitle());
+            ((HotViewHolder)holder).up.setText(item.getUpName());
+            ((HotViewHolder)holder).time.setText(DensityUtil.timeParse(item.getLength()));
+            ((HotViewHolder)holder).playNum.setText(String.valueOf(item.getPlayNum()));
+            ((HotViewHolder)holder).hot_video_cover.setTag(position);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mHeaderView == null && mFooterView == null) {
+            return TYPE_NORMAL;
+        }
+        if (position == 0) {
+            //第一个item应该加载Header
+            return TYPE_HEADER;
+        }
+        return TYPE_NORMAL;
+    }
+
+    public class HotViewHolder extends RecyclerView.ViewHolder {
+        private RoundCornerImageView video_cover;
+        private RelativeLayout hot_video_cover;
+        private TextView time, title, up, playNum;
+
+        public HotViewHolder(@NonNull View itemView) {
+            super(itemView);
+            video_cover = itemView.findViewById(R.id.Ranking_video_img);
+            hot_video_cover = itemView.findViewById(R.id.Hot_video_cover);
+            time = itemView.findViewById(R.id.Ranking_video_time);
+            title = itemView.findViewById(R.id.Ranking_video_Title);
+            up = itemView.findViewById(R.id.Ranking_video_up);
+            playNum = itemView.findViewById(R.id.Ranking_video_play_num);
+            hot_video_cover.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        position = (int) v.getTag();
+                        listener.onClick(position, getData().get(position).getId());
+                    }
                 }
-            }
-        });
-        if (item != null) {
-            Glide.with(mContext).load(item.getCover()).into(video_cover);
-            holder.text(R.id.Ranking_video_Title,item.getTitle());
-            holder.text(R.id.Ranking_video_up,item.getUpName());
-            holder.text(R.id.Ranking_video_time,"2.25");
-            holder.text(R.id.Ranking_video_play_num,""+item.getPlayNum());
+            });
+        }
+    }
+
+    public class HotHeaderViewHolder extends RecyclerView.ViewHolder{
+        private RelativeLayout rankingList;
+
+        public HotHeaderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            rankingList = itemView.findViewById(R.id.Ranking_List);
+            rankingList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    position = (int) v.getTag();
+                    ActivityUtils.startActivity(RankingLstActivity.class);
+                }
+            });
         }
     }
 
     public interface ItemOnClickListener {
 
-        void onClick(int position,int vid);
+        void onClick(int position, int vid);
 
     }
 }
